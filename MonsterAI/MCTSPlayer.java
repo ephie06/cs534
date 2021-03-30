839a179a47cac2da6243b8e4876273b6e9f0ae70
xudufy <xudufy@qq.com>
2021-03-30 19:15:13 -0400

    mcts w/o test

--- MonsterAI/MCTSPlayer.java
+++ MonsterAI/MCTSPlayer.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.function.Function;
+import java.util.stream.Collectors;
 
 class MCTSNode extends State {
 	
@@ -15,28 +16,32 @@ class MCTSNode extends State {
 	MCTSNode parent = null;
 	Card prevStep = null;
 	
-	//This is filter list for estimate the hand that can be played by player i, every filter function will filter the possible cards by one rule.
-	//TODO Not Implemented.
-	ArrayList<Function<Card, Boolean>> HandFilterPlayer1 = new ArrayList<>(), HandFilterPlayer3 = new ArrayList<>();
-	
 	MCTSNode(Deck deck, ArrayList<Card> round, ArrayList<Integer> scores, int index, ArrayList<Card> hand, MCTSNode parent) {
 		super(deck, round, scores, index);
 		// TODO Auto-generated constructor stub
 		this.hand = new ArrayList<>(hand);
+		this.parent = parent;
 		rng = ThreadLocalRandom.current();
+		fillPossibleMoves();
 	}
 	
 	MCTSNode (State secondCopy, ArrayList<Card> hand, MCTSNode parent) {
 		super(secondCopy);
 		this.hand = new ArrayList<>(hand);
 		this.targetIndex = playerIndex;
+		this.parent = parent;
 		rng = ThreadLocalRandom.current();
+		fillPossibleMoves();
 	}
 	
 	
 	MCTSNode (MCTSNode secondCopy, MCTSNode parent) {
 		super(secondCopy);
+		this.hand = new ArrayList<>(secondCopy.hand);
+		this.targetIndex = playerIndex;
+		this.parent = parent;
 		rng = ThreadLocalRandom.current();
+		fillPossibleMoves();
 	}
 	
 	public double meanValue() {
@@ -63,28 +68,90 @@ class MCTSNode extends State {
 		return playerScores;
 	}
 	
+	// Given a suit, check if the hand has that suit
+	boolean checkSuit(Suit check) {
+		boolean flag = false;
+		if (check == null) return false;
+		for (Card c: hand) { if (c.getSuit() == check) flag = true; }
+		return flag;
+	}
+
+	// Get the first suit that was played this round
+	Suit getFirstSuit(ArrayList<Card> currentRound) {
+		if (currentRound.size() == 0) return null;
+		return currentRound.get(0).getSuit();
+	}
+	
+	@Override
+	boolean isGameValid() {
+		return super.isGameValid() && playerScores.stream().allMatch(i->i<200);
+	}
+	
+	private void fillPossibleMoves() {
+		possibleMoves.clear();
+		if (!isGameValid()) {
+			return; 
+		}
+		
+		if (playerIndex == targetIndex) {
+			Suit firstSuit = getFirstSuit(currentRound);
+			// If no cards were played this round, play a random card 
+			if (firstSuit == null) {
+				possibleMoves.addAll(hand);
+			} else {
+				if (checkSuit(firstSuit)) {
+					possibleMoves.addAll(hand.stream().filter(i->(i.getSuit()==firstSuit)).collect(Collectors.toList()));
+				} else {
+					possibleMoves.addAll(hand);
+				}
+			}
+		} else {
+			possibleMoves.addAll(cardsPlayed.invertDeck);
+			for (var c:hand) {
+				possibleMoves.remove(c);
+			}
+		}
+		Collections.shuffle(possibleMoves, rng);
+	}
+	
+	private void makeOneMove(Card c) {
+		prevStep = c;
+		playCard(c, this.playerIndex);
+		if (validRound()) {
+			playerIndex = (playerIndex+1) % playerScores.size();
+		} else {
+			// Round has ended -- check what points have gone where and determine who goes next (use playerScores)
+			int firstPlayer = (playerIndex + 1) % playerScores.size();
+			int points = calculatePoints();
+			int taker = findTaker(firstPlayer);
+			playerScores.set(taker, playerScores.get(taker)+points);
+			
+			// Clear the cards on the table (don't worry, pointers to them are tracked in the cardsPlayed deck)
+			currentRound.clear();
+
+			// If game end.
+			if (!isGameValid()) {
+				playerScores.set(taker, playerScores.get(taker)+3); // give a expectation of the score of undealt cards.
+			}
+			
+			playerIndex = taker;
+		}
+		fillPossibleMoves();
+		
+	}
+	
 	MCTSNode selection() {
-		if (!visited || possibleMoves.size()!=0) {
+		if (possibleMoves.size()!=0) {
+			return this;
+		}
+		if (children.size()==0) {
 			return this;
 		}
 		return children.stream().max(Comparator.comparingDouble(i->i.UCB95())).get().selection();
 	}
 	
-	private void fillPossibleMoves() {
-		//TODO
-	}
-	
-	public void makeOneMove(Card c) {
-		prevStep = c;
-		//TODO
-	}
-	
 	MCTSNode expansion() {
-		if (visited!=false) {
-			fillPossibleMoves();
-			visited = true;
-		}
-		
+
 		if (possibleMoves.size()==0) {
 			return this;
 		}
@@ -106,8 +173,11 @@ class MCTSNode extends State {
 	}
 	
 	ArrayList<Integer> simulation() {
-		//TODO
-		return playerScores;
+		MCTSNode temp = new MCTSNode(this, this);
+		while(temp.possibleMoves.size()!=0) {
+			temp.makeOneMove(temp.possibleMoves.get(0)); //possibleMoves already shuffled.
+		}
+		return temp.terminalValue();
 	}
 	
 	public boolean equals(State obj) {
@@ -164,6 +234,7 @@ public class MCTSPlayer extends Player {
 				root = new MCTSNode(masterCopy, hand, null);
 			}
 		}
+		root.parent = null;
 		
 		long start = System.currentTimeMillis();
 		
