da4fc720baa79b7f4c13e1d43ed282ae36e5076e
xudufy <xudufy@qq.com>
2021-04-03 03:53:07 -0400

    update

--- MonsterAI/Monster.java
+++ MonsterAI/Monster.java
@@ -9,11 +9,11 @@ public class Monster {
 
 				// Initalize the deck of cards
 				Deck thing = new Deck();
-//				thing.printDeck();
+
 				// Assume this order is clockwise
-				Player p1 = new GDPlayer("Random1");
-				Player p2 = new MCTSPlayer("MCTS", 500);
-				Player p3 = new GDPlayer("Random3");
+				Player p1 = new RandomPlayAI("Random1");
+				Player p2 = new NewRolloutPlayer("NRP", 100);
+				Player p3 = new RandomPlayAI("Random3");
 
 				// at the end of every
 				// game, we will have all the cards back in the deck
@@ -22,18 +22,17 @@ public class Monster {
 				// Play Multiple Games
 				int numberOfGames = 1;
 				Game round = new Game(thing, p1, p2, p3);
-				round.debug=true;
 				while (!round.gameOver()) {
-///*					System.out.println("\n--------------------------------------------");
-//					System.out.println("--------------------------------------------");
-//					System.out.println("--------------------------------------------");
-//					System.out.println("Playing Game #"+numberOfGames);
-//					System.out.println("--------------------------------------------");
-//					System.out.println("--------------------------------------------");
-//					System.out.println("--------------------------------------------\n");*/
+/*					System.out.println("\n--------------------------------------------");
+					System.out.println("--------------------------------------------");
+					System.out.println("--------------------------------------------");
+					System.out.println("Playing Game #"+numberOfGames);
+					System.out.println("--------------------------------------------");
+					System.out.println("--------------------------------------------");
+					System.out.println("--------------------------------------------\n");*/
 					round.playNewGame();
 					numberOfGames++;
-					round.printTotalPoints();
+
 				}
 			}
 			catch (Exception e) {
@@ -45,7 +44,7 @@ public class Monster {
 
 	public static void main(String[] args) {
 
-		int n = 1; // Number of threads
+		int n = 8; // Number of threads
 		for (int i = 0; i < n; i++) {
 			MultithreadingDemo object = new MultithreadingDemo();
 			object.start();
@@ -57,10 +56,11 @@ public class Monster {
 
 		// Assume this order is clockwise
 		Player p1 = new RandomPlayAI("Random1");
-		Player p2 = new MCTSPlayer("MCTS Player", 1000);
+		Player p2 = new NewRolloutPlayer("NRP", 100);
 		Player p3 = new RandomPlayAI("Random3");
 
-		// at the end of every game, we will have all the cards back in the deck
+		// at the end of every
+		// game, we will have all the cards back in the deck
 		// thing.printDeck();
 
 		// Play Multiple Games
@@ -77,7 +77,7 @@ public class Monster {
 			round.playNewGame();
 			numberOfGames++;
 		
-		}
+		}*/
 		
 
 	}
